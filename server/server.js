ecfba29a6b415f8a9d4df415f024a87dab5a9784
unknown <ifratellidelweb@gmail.com>
2022-11-15 21:35:46 +0100

    Heroku fix

--- server/server.js
+++ server/server.js
@@ -47,6 +47,16 @@ const PORT = 5000;
 
 connectDB();
 
+const path = require('path');
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../client/build')))
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../client/build/index.html'))
+})
+
 const server = app.listen(PORT, () =>
   console.log(`Server Connected to port ${PORT}`)
 )
