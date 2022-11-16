b13010349d6800ba64caf07565a72e72ca48a2fb
unknown <ifratellidelweb@gmail.com>
2022-11-16 12:52:13 +0100

    Heroku fix

--- Auth/Route.js
+++ Auth/Route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, update, deleteUser } = require("./auth");
+const { register, login, update, deleteUser } = require("./Auth");
 const { adminAuth } = require("../middleware/auth")
 router.route("/register").post(register)
 router.route("/login").post(login);
