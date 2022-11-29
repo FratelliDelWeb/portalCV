4591d14891ccfa02f89567def01feeb2241fc481
unknown <ifratellidelweb@gmail.com>
2022-11-29 23:14:56 +0100

    Heroku Twillio

--- api/Call/route.js
+++ api/Call/route.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { makeCall, addCallerID, returnTwiml } = require("./call");
 router.route('/api/call').post(makeCall);
-router.route('/api/call/request').get(returnTwiml);
+router.route('/api/call/request').post(returnTwiml);
 /* router.route('/api/call/antonioM').post(addCallerID); */
 module.exports = router;
