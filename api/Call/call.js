1c5cf32c364a6d53252f9c4575360777d1738812
unknown <ifratellidelweb@gmail.com>
2022-11-30 12:43:08 +0100

    Heroku Twillio

--- api/Call/call.js
+++ api/Call/call.js
@@ -11,7 +11,6 @@ exports.makeCall = async (req, res, next) => {
     try{
         client.calls
         .create({
-            url: "https://handler.twilio.com/twiml/EH3e70a051af8cab6d6322e861df62a06c",
             to: '+393519907274',
             from: from,
             timeout: 15
